a410f52a8a4f5ac5fe696ca2f99f10b3a29e200d
Sun Jing <sunjingup@163.com>
2018-04-23 15:04:27 +0700

    array

---
+++ LeetCode/Array/maxProfit.h
@@ -0,0 +1,58 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+*/
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size()==0)
+            return 0;
+        int max=0;
+        for(int i=1;i<prices.size();i++){
+            int diff=prices[i]-prices[i-1];
+        if (diff>0)
+            max+=diff;
+        }
+        return max;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> prices = stringToIntegerVector(line);
+        
+        int ret = Solution().maxProfit(prices);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
